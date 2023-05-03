3300e73ee11fa8947c16eccd81cf0c62d2ca5d68
Chanyoung Lee <btstar1998@gmail.com>
2023-05-03 18:20:41 +0900

    Implement row-level parallelism among ranks

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -10,4 +10,23 @@
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
+  MPI_Scatter(
+    A, M * K / mpi_world_size, MPI_FLOAT,
+    &A[M * K / mpi_world_size * mpi_rank], M * K / mpi_world_size, MPI_FLOAT,
+    0, MPI_COMM_WORLD);
+  MPI_Bcast(B, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  for (int i = M / mpi_world_size * mpi_rank; i < M / mpi_world_size * (mpi_rank + 1); i++) {
+    // printf("%d, %d\n", mpi_rank, i);
+    for (int j = 0; j < N; j++) {
+      float sum = 0;
+      for (int k = 0; k < K; k++) sum += A[i * K + k] * B[k * N + j];
+      C[i * N + j] = sum;
+    }
+  }
+
+  MPI_Gather(
+    &C[M / mpi_world_size * mpi_rank * N], M * N / mpi_world_size, MPI_FLOAT,
+    C, M * N / mpi_world_size, MPI_FLOAT,
+    0, MPI_COMM_WORLD);
 }
