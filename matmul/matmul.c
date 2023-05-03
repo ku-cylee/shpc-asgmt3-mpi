8e8e5e203e202d6fa7d3c570fd9eefc41ec4faa9
Chanyoung Lee <btstar1998@gmail.com>
2023-05-03 20:17:43 +0900

    Implement thread parallelism over j

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -10,17 +10,22 @@
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
-  MPI_Scatter(
-    A, M * K / mpi_world_size, MPI_FLOAT,
-    &A[M * K / mpi_world_size * mpi_rank], M * K / mpi_world_size, MPI_FLOAT,
-    0, MPI_COMM_WORLD);
+  // MPI_Scatter(
+  //   A, M * K / mpi_world_size, MPI_FLOAT,
+  //   &A[M * K / mpi_world_size * mpi_rank], M * K / mpi_world_size, MPI_FLOAT,
+  //   0, MPI_COMM_WORLD);
+  MPI_Bcast(A, M * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(B, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
   for (int i = M / mpi_world_size * mpi_rank; i < M / mpi_world_size * (mpi_rank + 1); i++) {
-    for (int j = 0; j < N; j++) {
-      float sum = 0;
-      for (int k = 0; k < K; k++) sum += A[i * K + k] * B[k * N + j];
-      C[i * N + j] = sum;
+    #pragma omp parallel num_threads(threads_per_process) shared(A, B, i, K, N)
+    {
+      #pragma omp for
+      for (int j = 0; j < N; j++) {
+        float sum = 0;
+        for (int k = 0; k < K; k++) sum += A[i * K + k] * B[k * N + j];
+        C[i * N + j] = sum;
+      }
     }
   }
 
