9560ece166c86783ebd28a6d9fab5b4a1d6da9d7
Chanyoung Lee <btstar1998@gmail.com>
2023-05-05 04:28:23 +0900

    Refactor code, Initialize B_vecs when traversing

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -7,13 +7,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TILE_SIZE   16
+
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
 
-  int i, j;
+  int i, j, k, t;
   int M_per_rank = M / mpi_world_size;
   int M_start = M_per_rank * mpi_rank;
+  int VEC_K = K / TILE_SIZE;
 
   // MPI_Scatter(
   //   A, M * K / mpi_world_size, MPI_FLOAT,
@@ -22,28 +25,27 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
   MPI_Bcast(A, M * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(B, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  __m512 *A_vecs = (__m512 *)aligned_alloc(32, M_per_rank * K / 16 * sizeof(__m512));
-  for (i = 0; i < M_per_rank * K / 16; i++) {
-    A_vecs[i] = _mm512_load_ps(&A[M_start * K + i * 16]);
+  __m512 *A_vecs = (__m512 *)aligned_alloc(32, M_per_rank * VEC_K * sizeof(__m512));
+  for (i = 0; i < M_per_rank * VEC_K; i++) {
+    A_vecs[i] = _mm512_load_ps(&A[M_start * K + i * TILE_SIZE]);
   }
 
-  __m512 *B_vecs = (__m512 *)aligned_alloc(32, K * N / 16 * sizeof(__m512));
-  for (j = 0; j < N; j++) {
-    for (int k = 0; k < K / 16; k++) {
-      float seg[16];
-      for (int t = 0; t < 16; t++) seg[t] = B[(k * 16 + t) * N + j];
-      B_vecs[k * N + j] = _mm512_load_ps(seg);
-    }
-  }
+  __m512 *B_vecs = (__m512 *)aligned_alloc(32, VEC_K * N * sizeof(__m512));
 
-  #pragma omp parallel num_threads(threads_per_process) shared(j)
+  #pragma omp parallel num_threads(threads_per_process) shared(C, j)
   {
-    #pragma omp for private(i)
+    #pragma omp for private(i, k, t)
     for (j = 0; j < N; j++) {
+      for (k = 0; k < VEC_K; k++) {
+        float seg[TILE_SIZE];
+        for (t = 0; t < TILE_SIZE; t++) seg[t] = B[(k * TILE_SIZE + t) * N + j];
+        B_vecs[k * N + j] = _mm512_load_ps(seg);
+      }
+
       for (i = 0; i < M_per_rank; i++) {
         __m512 C_vec = _mm512_setzero_ps();
-        for (int k = 0; k < K / 16; k++) {
-          __m512 A_vec = A_vecs[i * K / 16 + k];
+        for (k = 0; k < VEC_K; k++) {
+          __m512 A_vec = A_vecs[i * VEC_K + k];
           __m512 B_vec = B_vecs[k * N + j];
           C_vec = _mm512_fmadd_ps(A_vec, B_vec, C_vec);
         }
