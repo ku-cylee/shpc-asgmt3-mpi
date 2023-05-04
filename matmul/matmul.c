92edc22950c3a955577196ca1ed17b8662acce70
Chanyoung Lee <btstar1998@gmail.com>
2023-05-05 02:03:34 +0900

    Use m512 vector matrix to accumulate inner products

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -10,30 +10,46 @@
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
+
+  int i, j;
+  int M_per_rank = M / mpi_world_size;
+  int M_start = M_per_rank * mpi_rank;
+
   // MPI_Scatter(
   //   A, M * K / mpi_world_size, MPI_FLOAT,
   //   &A[M * K / mpi_world_size * mpi_rank], M * K / mpi_world_size, MPI_FLOAT,
   //   0, MPI_COMM_WORLD);
   MPI_Bcast(A, M * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(B, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  __m512 *C_vecs = (__m512 *)aligned_alloc(32, M_per_rank * N * sizeof(__m512));
+  for (int i = 0; i < M_per_rank * N; i++) C_vecs[i] = _mm512_setzero_ps();
   
-  int j;
-  #pragma omp parallel num_threads(threads_per_process) shared(C, j)
+  #pragma omp parallel num_threads(threads_per_process) shared(C_vecs, j)
   {
-    #pragma omp for
+    #pragma omp for private(i)
     for (j = 0; j < N; j++) {
       for (int k = 0; k < K; k += 16) {
         float B_seg[16];
         for (int kk = 0; kk < 16; kk++) B_seg[kk] = B[(k + kk) * N + j];
         __m512 B_vec = _mm512_load_ps(B_seg);
-        for (int i = M / mpi_world_size * mpi_rank; i < M / mpi_world_size * (mpi_rank + 1); i++) {
-          __m512 sum_vec = _mm512_setzero_ps();
-          __m512 A_vec = _mm512_load_ps(&A[i * K + k]);
-          sum_vec = _mm512_fmadd_ps(A_vec, B_vec, sum_vec);
-
-          float *sum_arr = (float *)&sum_vec;
-          for (int kk = 0; kk < 16; kk++) C[i * N + j] += sum_arr[kk];
+        for (int i = 0; i < M_per_rank; i++) {
+          __m512 A_vec = _mm512_load_ps(&A[(i + M_start) * K + k]);
+          int idx = i * N + j;
+          C_vecs[idx] = _mm512_fmadd_ps(A_vec, B_vec, C_vecs[idx]);
         }
+      }
+    }
+  }
+
+  #pragma omp parallel num_threads(threads_per_process) shared(C, i)
+  {
+    #pragma omp for private(j)
+    for (i = 0; i < M_per_rank; i++) {
+      for (j = 0; j < N; j++) {
+        float C_arr[16] __attribute__((aligned(32)));
+        _mm512_store_ps(C_arr, C_vecs[i * N + j]);
+        C[(i + M_start) * N + j] = _mm512_reduce_add_ps(C_vecs[i * N + j]);
       }
     }
   }
