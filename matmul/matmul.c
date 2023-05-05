5fdb289eaf07d49f7566ef8dd7c40f5564c61aab
Chanyoung Lee <btstar1998@gmail.com>
2023-05-05 23:11:44 +0900

    Implement tiling

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -7,13 +7,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TILE_SIZE   64
 #define VECTOR_SIZE 16
 
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
 
-  int i, j, k, v;
+  int i, j, jj, k, kk, v;
   int M_per_rank = M / mpi_world_size;
   int M_start = M_per_rank * mpi_rank;
   int VEC_K = K / VECTOR_SIZE;
@@ -26,30 +27,40 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
   MPI_Bcast(B, K * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
   __m512 *A_vecs = (__m512 *)aligned_alloc(32, M_per_rank * VEC_K * sizeof(__m512));
-  for (i = 0; i < M_per_rank * VEC_K; i++) {
-    A_vecs[i] = _mm512_load_ps(&A[M_start * K + i * VECTOR_SIZE]);
+  #pragma omp parallel num_threads(threads_per_process) shared(i)
+  {
+    #pragma omp for
+    for (i = 0; i < M_per_rank * VEC_K; i++) {
+      A_vecs[i] = _mm512_load_ps(&A[M_start * K + i * VECTOR_SIZE]);
+    }
   }
 
   __m512 *B_vecs = (__m512 *)aligned_alloc(32, VEC_K * N * sizeof(__m512));
 
-  #pragma omp parallel num_threads(threads_per_process) shared(C, j)
+  #pragma omp parallel num_threads(threads_per_process) shared(j)
   {
-    #pragma omp for private(i, k, v)
-    for (j = 0; j < N; j++) {
-      for (k = 0; k < VEC_K; k++) {
-        float b[VECTOR_SIZE];
-        for (v = 0; v < VECTOR_SIZE; v++) b[v] = B[(k * VECTOR_SIZE + v) * N + j];
-        B_vecs[k * N + j] = _mm512_load_ps(b);
+    #pragma omp for private(i, jj, k, kk, v)
+    for (j = 0; j < N; j += TILE_SIZE) {
+      for (jj = 0; jj < TILE_SIZE; jj++) {
+        for (k = 0; k < VEC_K; k++) {
+          float b[VECTOR_SIZE];
+          for (v = 0; v < VECTOR_SIZE; v++) b[v] = B[(k * VECTOR_SIZE + v) * N + j + jj];
+          B_vecs[k * N + j + jj] = _mm512_load_ps(b);
+        }
       }
 
-      for (i = 0; i < M_per_rank; i++) {
-        __m512 c = _mm512_setzero_ps();
-        for (k = 0; k < VEC_K; k++) {
-          __m512 a = A_vecs[i * VEC_K + k];
-          __m512 b = B_vecs[k * N + j];
-          c = _mm512_fmadd_ps(a, b, c);
+      for (k = 0; k < VEC_K; k += TILE_SIZE) {
+        for (i = 0; i < M_per_rank; i++) {
+          for (jj = 0; jj < TILE_SIZE; jj++) {
+            __m512 c = _mm512_setzero_ps();
+            for (kk = 0; kk < TILE_SIZE; kk++) {
+              __m512 a = A_vecs[i * VEC_K + k + kk];
+              __m512 b = B_vecs[(k + kk) * N + j + jj];
+              c = _mm512_fmadd_ps(a, b, c);
+            }
+            C[(i + M_start) * N + j + jj] += _mm512_reduce_add_ps(c);
+          }
         }
-        C[(i + M_start) * N + j] = _mm512_reduce_add_ps(c);
       }
     }
   }
